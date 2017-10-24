7a73cb0e1dc90b3944a8695c40428bc8b6df7028
Easwaran.G <rajkumar26041997@gmail.com>
2017-10-24 13:06:47 +0530

    Run server before running client

--- src/calculatorrmi/CalculatorServer.java
+++ src/calculatorrmi/CalculatorServer.java
@@ -11,7 +11,7 @@ import java.rmi.registry.Registry;
 
 /**
  *
- * @author Easwaran
+ * @author EASWARAN
  */
 public class CalculatorServer {
     public static void main(String [] args) throws RemoteException{
